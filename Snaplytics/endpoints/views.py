7669b8cccc5216b38521acf8fd36876d66f883dd
jevvii <jayveemarcelo09@gmail.com>
2026-03-02 07:07:39 +0800

    Optimized for Windows

--- Snaplytics/endpoints/views.py
+++ Snaplytics/endpoints/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0a29a156e54c3bd56519a8c5d201deeb68bd413b7c75e6cc75e9e306f30c9fd
-size 63584
+oid sha256:11496bd48bdaa346ae1f3a80c8769243d210e2421cbbb282a4acd11843f5b090
+size 63564
